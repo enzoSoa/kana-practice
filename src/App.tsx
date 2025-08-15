e19d79231ce929dcdc88ecb00225fab7553b8630
Enzo Ayrton Soares <enzoayrsoares@gmail.com>
2025-08-15 14:52:08 +0200

    Add card switch animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import { useMemo, useState } from 'react'
+import { useReducer } from 'react'
 import { Card } from './components'
 import { useRandomKana } from './hooks';
 import './App.css'
 
 
-
 function App() {
   const {hiraganaToRomanji, getRandomKana} = useRandomKana();
-  const [currentKana, setCurrentKana] = useState(getRandomKana());
-  const targetRomaji = useMemo(() => hiraganaToRomanji[currentKana], [currentKana, hiraganaToRomanji])
+  const [kanaList, dispatch] = useReducer((state, data) => {
+    switch (data) {
+      case 'add-kana':
+        return [...state, getRandomKana()];
+      case 'remove-first-kana':
+        return state.slice(1)
+    }
+  }, [getRandomKana()]);
 
-  return <Card kana={currentKana} target={targetRomaji} onGoodGuess={() => setTimeout(() => setCurrentKana(getRandomKana()), 300)}/>
+  const handleGoodGuess = () => {
+    dispatch('add-kana');
+    setTimeout(() => dispatch('remove-first-kana'), 300);
+  }
+
+  return <div className='app'>
+    {kanaList.map(kana => 
+      <Card key={kana} kana={kana} target={hiraganaToRomanji[kana]} onGoodGuess={handleGoodGuess}/>
+    )}
+  </div>
 }
 
 export default App
